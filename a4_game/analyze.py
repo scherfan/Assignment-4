cafbb6eceeee68f97d76248230182b8fada59dcc
scherfan <cherfan@ualberta.ca>
2014-03-13 19:24:27 -0600

    got closer with analyze.py

--- a4_game/analyze.py
+++ a4_game/analyze.py
@@ -1,5 +1,7 @@
 def destroy_prob(attacker, defender, current_tile, turns):
     prob = list()
-    prob[turns] = d
-    d = attacker.get_damage(defender, current_tile)
-    return prob[turns]
+    for i in range(turns + 1):
+        d = attacker.get_damage(defender, current_tile)
+        prob.append(i/((d+0) * (d+0) * (d+1) * (d+2)))
+
+    return prob
