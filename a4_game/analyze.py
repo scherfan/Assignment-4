b5fc22806c5d54b86e083364672c1d3195814a70
scherfan <cherfan@ualberta.ca>
2014-03-13 20:38:34 -0600

    Got analyze to work properly(maybe)

--- a4_game/analyze.py
+++ a4_game/analyze.py
@@ -1,7 +1,7 @@
 def destroy_prob(attacker, defender, current_tile, turns):
     prob = list()
     for i in range(turns + 1):
-        d = attacker.get_damage(defender, current_tile)
-        prob.append(i/((d+0) * (d+0) * (d+1) * (d+2)))
-
+        d = attacker.get_damage(defender, current_tile) # get attacker damage
+        prob.append(((d * 0.2) * i)/defender.health) # append probability at that turn to prob
+        
     return prob
